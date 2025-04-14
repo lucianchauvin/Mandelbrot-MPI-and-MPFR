cf3240a869716e21743a5f74813425c447d49b0c
Lucian Chauvin <lucianchauvin@gmail.com>
2025-04-13 21:54:28 -0500

    easy optimization

--- mandel.c
+++ mandel.c
@@ -58,8 +58,8 @@ int main (int argc, char *argv[]){
     clock_t begin, end;
     double time_spent;
 
-    mpfr_t x_center, y_center, x_pix, y_pix, x2, y2, x2y2, x, y, zoom_factor, frame_skip;
-    mpfr_inits2  (precision, x_center, y_center, x_pix, y_pix, x, y, x2, y2, x2y2, zoom_factor, frame_skip, (mpfr_ptr) 0);
+    mpfr_t x_center, y_center, x_pix, y_pix, x_pix_d, y_pix_d, x2, y2, x2y2, x, y, zoom_factor, frame_skip;
+    mpfr_inits2  (precision, x_center, y_center, x_pix, y_pix, x_pix_d, y_pix_d, x, y, x2, y2, x2y2, zoom_factor, frame_skip, (mpfr_ptr) 0);
     mpfr_set_str (x_center, x_center_s, 10, round_mode);
     mpfr_set_str (y_center, y_center_s, 10, round_mode);
     mpfr_set_ui  (frame_skip, frame_skip_init, round_mode);
@@ -165,13 +165,13 @@ int main (int argc, char *argv[]){
         pack_buffer_pos = 0;
         packed_msg_s = 0;
 
-        MPI_Recv        (pack_buffer, 1000, MPI_PACKED, PARENT, 0, MPI_COMM_WORLD, &status);
-        MPI_Get_count   (&status, MPI_BYTE, &packed_msg_s);
-        MPI_Unpack      (pack_buffer, 1000, &pack_buffer_pos, &current_frame, 1, MPI_INT, MPI_COMM_WORLD);
-        MPI_Unpack      (pack_buffer, 1000, &pack_buffer_pos, &max_iter, 1, MPI_INT, MPI_COMM_WORLD);
+        MPI_Recv      (pack_buffer, 1000, MPI_PACKED, PARENT, 0, MPI_COMM_WORLD, &status);
+        MPI_Get_count (&status, MPI_BYTE, &packed_msg_s);
+        MPI_Unpack    (pack_buffer, 1000, &pack_buffer_pos, &current_frame, 1, MPI_INT, MPI_COMM_WORLD);
+        MPI_Unpack    (pack_buffer, 1000, &pack_buffer_pos, &max_iter, 1, MPI_INT, MPI_COMM_WORLD);
         printf("Worker %d recived frame %d\n", rank, current_frame);
 
-        MPI_Unpack      (pack_buffer, 1000, &pack_buffer_pos, unpack_stream_buffer, packed_msg_s - pack_buffer_pos, MPI_BYTE, MPI_COMM_WORLD);
+        MPI_Unpack    (pack_buffer, 1000, &pack_buffer_pos, unpack_stream_buffer, packed_msg_s - pack_buffer_pos, MPI_BYTE, MPI_COMM_WORLD);
         stream = fmemopen(unpack_stream_buffer, 1000, "r");
         for(i = 0; i < 4; ++i){
             mpfr_fpif_import(bounds[i], stream);
@@ -191,36 +191,34 @@ int main (int argc, char *argv[]){
         }
         fprintf(fp, "P6\n%d %d\n255\n", (int) rez_width, (int) rez_height);
 
+        /* x_pix_d = (bounds[2] - bounds[0])/rez_width); */ 
+        mpfr_sub   (x_pix_d, bounds[2], bounds[0], round_mode);
+        mpfr_div_d (x_pix_d, x_pix_d, rez_width, round_mode);
+
+        /* y_pix_d = (bounds[1] - bounds[3])/rez_height); */ 
+        mpfr_sub   (y_pix_d, bounds[1], bounds[3], round_mode);
+        mpfr_div_d (y_pix_d, y_pix_d, rez_height, round_mode);
+
+        mpfr_set (y_pix, bounds[3], round_mode);
+
         begin = clock();
         for (j = 0; j < (int) rez_height; ++j){
+            mpfr_set (x_pix, bounds[0], round_mode);
+            mpfr_add (y_pix, y_pix, y_pix_d, round_mode);
             for (i = 0; i < (int) rez_width; ++i){   
-                /* x_pix = bounds[0] + (bounds[2] - bounds[0])*(i/rez_width); */ 
-                mpfr_sub   (x_pix, bounds[2], bounds[0], round_mode);
-                mpfr_mul_d (x_pix, x_pix, i, round_mode);
-                mpfr_div_d (x_pix, x_pix, rez_width, round_mode);
-                mpfr_add   (x_pix, x_pix, bounds[0], round_mode);
+                mpfr_add (x_pix, x_pix, x_pix_d, round_mode);
 
-                /* y_pix = bounds[3] + (bounds[1] - bounds[3])*(j/rez_height); */ 
-                mpfr_sub   (y_pix, bounds[1], bounds[3], round_mode);
-                mpfr_mul_d (y_pix, y_pix, j, round_mode);
-                mpfr_div_d (y_pix, y_pix, rez_height, round_mode);
-                mpfr_add   (y_pix, y_pix, bounds[3], round_mode);
-
-                /* x = 0; */
-                mpfr_set_ui (x, 0, round_mode);
-                /* y = 0; */
-                mpfr_set_ui (y, 0, round_mode);
-                /* x2 = 0; */
-                mpfr_set_ui (x2, 0, round_mode);
-                /* y2 = 0; */
-                mpfr_set_ui (y2, 0, round_mode);
-                /* x2y2 = 0; */
-                mpfr_set_ui (x2y2, 0, round_mode);
+                // zero everything
+                mpfr_set_zero (x, 1);
+                mpfr_set_zero (y, 1);
+                mpfr_set_zero (x2, 1);
+                mpfr_set_zero (y2, 1);
+                mpfr_set_zero (x2y2, 1);
 
                 /* SCOREP_USER_REGION_DEFINE(inner_loop); */
                 /* SCOREP_USER_REGION_BEGIN(inner_loop, "test", SCOREP_USER_REGION_TYPE_LOOP); */
 
-                /* x2 + y2 <= 4 */
+                /* x2 + y2 <= escape_radius */
                 escape_i = 0;
                 while (mpfr_cmp_ui(x2y2, escape_radius) <= 0 && escape_i < max_iter){
                     /* y = 2*x*y+yVal; */
@@ -233,10 +231,11 @@ int main (int argc, char *argv[]){
                     mpfr_add     (x, x, x_pix, round_mode);
 
                     /* x2 = x*x; */
-                    mpfr_sqr     (x2, x, round_mode);
-
                     /* y2 = y*y; */
+                    mpfr_sqr     (x2, x, round_mode);
                     mpfr_sqr     (y2, y, round_mode);
+
+                    /* x2y2 = x2 + y2; */
                     mpfr_add     (x2y2, x2, y2, round_mode);
 
                     escape_i += 1;
